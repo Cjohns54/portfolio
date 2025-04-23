1ea43bb032209886da437abd627cedbab52f92ef
CjohnsonEDM <cjohnson@editable.com>
2025-04-23 14:35:09 -0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Box from './screens/Box/Box';
-import Contact from './screens/Contact/Contact';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from './screens/Box/Box';
+import { Contact } from './screens/Contact/Contact';
+import { HardwareProjects } from './screens/Projects/HardwareProjects';
+import { SmartDesk } from './screens/Projects/SmartDesk';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Box />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Box />} />
+      <Route path="/hardware-projects" element={<HardwareProjects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/smart-desk" element={<SmartDesk />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box } from "./screens/Box/Box";
-import { HardwareProjects } from "./screens/Projects/HardwareProjects";
-import { Contact } from "./screens/Contact/Contact";
-import { SmartDesk } from "./screens/Projects/SmartDesk";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<Box />} />
-        <Route path="/hardware-projects" element={<HardwareProjects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/smart-desk" element={<SmartDesk />} />
-      </Routes>
+      <App />
     </Router>
   </StrictMode>
 );
